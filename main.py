75134a71ef2d5c9c83fa20c07542de48499c92f8
studyHackSlab <138351574+studyHackSlab@users.noreply.github.com>
2023-07-02 14:41:42 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/")
+# @app.get("/")
+# def index():
+#     return {"Hello": "World"}
+
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@@app.route('/')
 def index():
-    return {"Hello": "World"}
+    return render_template('index.html', message="名前を入力してください。")
+
+@app.route('/result', methods=["GET"])
+def result_get():
+    # GET送信の処理
+    field = request.args.get("field","")
+    return render_template('result.html', message = "名前は{}です。".format(field))
+
+@app.route('/result', methods=["POST"])
+def result_post():
+    # POST送信の処理
+    field = request.form["field"]
+    return render_template('result.html', message = "名前は{}です。".format(field))
+
+if __name__ == '__main__':
+    app.debug = False
+    app.run(host='localhost')
