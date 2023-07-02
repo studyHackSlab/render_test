7071e41543fe888d5af65d7f88d6b0bd7f2b74e4
studyHackSlab <138351574+studyHackSlab@users.noreply.github.com>
2023-07-02 15:01:11 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -11,21 +11,25 @@ from flask import Flask, render_template,request
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html', message="名前を入力してください。")
 
-@app.route('/result', methods=["GET"])
-def result_get():
-    # GET送信の処理
-    field = request.args.get("field","")
-    return render_template('result.html', message = "名前は{}です。".format(field))
+def hello_world():
+    return "Hello World!"
 
-@app.route('/result', methods=["POST"])
-def result_post():
-    # POST送信の処理
-    field = request.form["field"]
-    return render_template('result.html', message = "名前は{}です。".format(field))
+# def index():
+#     return render_template('index.html', message="名前を入力してください。")
 
-if __name__ == '__main__':
-    app.debug = False
-    app.run(host='localhost')
+# @app.route('/result', methods=["GET"])
+# def result_get():
+#     # GET送信の処理
+#     field = request.args.get("field","")
+#     return render_template('result.html', message = "名前は{}です。".format(field))
+
+# @app.route('/result', methods=["POST"])
+# def result_post():
+#     # POST送信の処理
+#     field = request.form["field"]
+#     return render_template('result.html', message = "名前は{}です。".format(field))
+
+# if __name__ == '__main__':
+#     app.debug = False
+#     app.run(host='localhost')
